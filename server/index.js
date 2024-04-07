bdb4284495d3f6839cb2203b1e5248fe935b9403
Diken <dikenmaharjan10@gmail.com>
2024-04-07 12:00:00 +0545

    feat: support verification in server

--- server/index.js
+++ server/index.js
@@ -32,12 +32,11 @@ app.post("/api/signIn", (req, res) => {
 			error_message: "Incorrect Credentials"
 		});
 	}
+	let user = result[0];
 	let code = generateCode();
-		userOtpCodes[newUser.id] = code;
+		userOtpCodes[user.id] = code;
 		console.log(`OTP sent to email ${email}`);
 		console.log(`The OTP is ${code}`)
-
-	let user = result[0];
 
 	res.json({
 			userName: user.userName,
@@ -96,14 +95,6 @@ app.post("/api/signUp", (req, res) => {
 	});
 });
 
-app.post("/api/verification", (req, res) => { 
-	if (code === req.body.code) {
-		return res.json({ message: "You're verified successfully" });
-	}
-	res.json({
-		error_message: "Incorrect credentials",
-	});
-});
 
 app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
